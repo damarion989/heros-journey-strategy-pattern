c9fc89aeedc9f95bc43f4ecd16ae763bb7bcc048
damarion989 <d.butlers282@gmail.com>
2026-08-23 16:40:16 -0500

    Fix override annotation in FlyingHero class

--- src/FlyingHero.java
+++ src/FlyingHero.java
@@ -1,6 +1,6 @@
 public class FlyingHero extends Hero {
-  Override
-  public void move() {
-    System.out.println("I am soaring through the clouds!");
-  }
+    @Override
+    public void move() {
+        System.out.println("I am soaring through the clouds!");
+    }
 }
